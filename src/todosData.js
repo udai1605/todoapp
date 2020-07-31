5e59cafa4b07f9d59f151fd97c976cbcd5d477a9
udai1605 <rudaikiran@gmail.com>
2020-07-31 19:38:54 +0530

    v 1.0

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Take out the trash",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Grocery shopping",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Clean water tank",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Pay Electricity Bill",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Book Tickets",
+        completed: false
+    }
+]
+
+export default todosData
